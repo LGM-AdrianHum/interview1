cbd5f9aed9e7cdc061ea04cecc4fc515dd67b44e
Adrian Hum <LGM-AdrianHum@users.noreply.github.com>
2023-07-21 14:11:30 +1000

    Update Program.cs to introduce "mistakes"
    Two issues have been added to the code... please correct the.

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -7,17 +7,14 @@ namespace Samples
 
         static bool IsPrime(long number)
         {
-            if (number <= 1)
-                return false;
+            if (number <= 1) return false;
 
-            if (number == 2 || number == 3)
-                return true;
+            if (number == 2 || number == 3) return true;
 
-            if (number % 2 == 0 || number % 3 == 0)
-                return false;
+            if (number % 2 == 0 || number % 3 == 0) return false;
 
-          
-            for (long i = 5; i <= number; i += 6)
+          //Question: Please fix this code... hint the initial value of i is wrong and so is the incrementer
+            for (long i = 1; i <= number; i += 1)
             {
                 if (number % i == 0 || number % (i + 2) == 0)
                     return false;
